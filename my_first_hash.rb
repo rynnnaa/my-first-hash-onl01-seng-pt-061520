f5a82ebe16197f940685a521d7c76601f06f4997
Ryna Truong <rynatruong@outlook.com>
2020-06-25 23:58:02 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -18,6 +18,8 @@ def retrieval
                   "porcelain vases" => 2, 
                   "oil paintings" => 3
                  }
+                 
+                 shipping_manifest["oil paintings"]
                 
                  
 
