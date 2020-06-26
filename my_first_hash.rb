340ae724a013f4a4f8205258102dced5f0da8852
Ryna Truong <rynatruong@outlook.com>
2020-06-26 00:04:47 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,7 +35,7 @@ def adding
                  }
                  
   shipping_manifest["muskets"] = 2
-  shipping_manifest["gun powder" = 4]
+  shipping_manifest["gun powder"] = 4
   # add 2 muskets to the shipping_manifest hash below
 
   # add 4 gun powder to the shipping_manifest hash below
